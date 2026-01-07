04cf8f6ebbcce5a161c9835124f2c87dcf19b4d2
Leslie Peterson <leslie.peterson@universitylake.org>
2026-01-07 16:15:20 -0600

    Initial Scripts template

---
+++ python_list_operations_MiniScriptActivity.py
@@ -0,0 +1,126 @@
+# Python List Operations - Group Activity
+# Slide due: Thu 1/8/26  •  Python Script due: Fri 1/9/26
+
+# Instructions:
+#   Code & Test: In VS Code or IDLE, write a script that fulfills each challenge in your packet.
+#   Test each (highlight the section of the code and hit 'run' - make your print statements descriptive).
+#   BONUS! Try using multiple of the below methods in addition to slicing, negative indexing, and/or step values.
+#   Submit: Upload your completed script to the assignment on GITHUB by the due date.
+# -----------------------------------------------------------------------------
+# NAME:
+# -----------------------------------------------------------------------------
+
+
+
+
+'''-----------------------------------------------------------------------------
+TASK 1: The append() Method (feat. len())
+Coding Prompt: Create a list of 3 items; print the length
+append a user input and show that len() increases by 1 with a second print statement
+-----------------------------------------------------------------------------'''
+
+
+
+
+''' -----------------------------------------------------------------------------
+TASK 2: The insert() Method
+Prompt: Create a 4-item list and insert a new item at index 1; 
+print the list to show positions changed.
+-----------------------------------------------------------------------------'''
+
+
+
+
+'''-----------------------------------------------------------------------------
+TASK 3: Concatenation (+) & Replication (*)
+Prompt:Define three variales: row = ['_'] * 3; add_on = ['X', 'O']; final_row = row + add_on; 
+print final_row.
+-----------------------------------------------------------------------------'''
+
+
+
+
+
+'''-----------------------------------------------------------------------------
+TASK 4: The extend() Method 
+Prompt: Create a list for a backpack, and for a loot pile (found_items), Use .extend() to 
+add the loot pile to your backpack, print the results.
+-----------------------------------------------------------------------------'''
+
+
+
+
+''' -----------------------------------------------------------------------------
+TASK 5: The remove() Method (feat. count()) 
+Prompt: Create a list with duplicates (e.g., [1,5,5,5,9]); 
+print the count before and after one remove(value).
+-----------------------------------------------------------------------------'''
+
+
+
+
+'''-----------------------------------------------------------------------------
+TASK 6: The pop() Method 
+Prompt: Create a vip_list; kicked_out = vip_list.pop(0); print removed name AND updated list. 
+-----------------------------------------------------------------------------'''
+
+
+
+
+''' -----------------------------------------------------------------------------
+TASK 7: The del Statement (Slicing) 🔪
+Prompt: Create list of numbers 1 through 10 and use del to remove a middle slice (e.g., del list[3:6]); 
+print result.
+-----------------------------------------------------------------------------'''
+
+
+
+
+'''-----------------------------------------------------------------------------
+ TASK 8: The index() Method (feat. keyword: in)
+ Prompt: Ask the user for an item; if it's in the list, print .index(), 
+ else print "Safe! That item is not in the list."
+-----------------------------------------------------------------------------'''
+
+
+
+
+''' -----------------------------------------------------------------------------
+TASK 9: Ordering (sort and reverse) 
+Prompt: Create 5 random numbers, sort() them, then reverse() and print final result (high to low). 
+-----------------------------------------------------------------------------'''
+
+
+
+
+''' -----------------------------------------------------------------------------
+TASK 10: The copy() Method
+Prompt: Create a list with many items. Then create another variable and copy the first list
+Use .clear() or del on the first list, print it to show it is empty
+print the copied backup list to prove data saved.
+-----------------------------------------------------------------------------'''
+
+
+
+
+'''# -----------------------------------------------------------------------------
+TASK 11: Random Selection (random.choice)
+Prompt: Create a list of Magic 8 Ball answers and call random.choice() three times, 
+printing each result.
+-----------------------------------------------------------------------------'''
+
+
+
+'''-----------------------------------------------------------------------------
+TASK 12: Nested Lists (Accessing 2D Data)
+Prompt: Create a list containing two smaller lists inside, e.g. matrix = [[1,2],[3,4]]; 
+print matrix[1][0] to show the number 3. Note: If you want to play around with other lists, go for it;
+Your main task here is to access a specific, single item WITHIN one of the lists inside the other list.
+-----------------------------------------------------------------------------'''
+
+
+
+'''-----------------------------------------------------------------------------
+BONUS: Use list slicing, negative indexing, and/or step values in addition to a selection of 2 or more
+of the above methods to create a more complex list operation. Print the results.
+-----------------------------------------------------------------------------'''
